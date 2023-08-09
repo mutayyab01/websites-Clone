7d39244b0930bcf570cc4320e546b90512bc1533
Mutayyab Imran <mutayyab786yo@gmail.com>
2023-08-09 11:52:00 +0500

    Update app.js

--- Game Animation/app.js
+++ Game Animation/app.js
@@ -9,7 +9,7 @@ console.log(right);
 document.addEventListener('keydown',function(){
 if(event.key==='ArrowRight'){
     thanosStand.style.width='25%'
-thanosStand.src='/images/thanos-walkf.gif';
+thanosStand.src='images/thanos-walkf.gif';
 if (event.key!=='a') {
    thanosStand.style.left =(right+=5) +'px' ;
 }}})
@@ -19,7 +19,7 @@ if(event.key==='ArrowRight'){
     //console.log(thanosStand.style.width);
     thanosStand.style.width=thanosWidth
 
-thanosStand.src='/images/thanos-msh-stance.gif';
+thanosStand.src='images/thanos-msh-stance.gif';
 }})
 
 
@@ -28,7 +28,7 @@ document.addEventListener('keydown',function(){
 if(event.key==='ArrowLeft'){
     thanosStand.style.width='25%'
 
-thanosStand.src='/images/than-walk-left.gif';
+thanosStand.src='images/than-walk-left.gif';
 if (event.key!=='a') {
     console.log(thanosStand.style.left =(right-=5) +'px' );
     if (right<0) {
@@ -38,7 +38,7 @@ document.addEventListener('keyup',function(){
 if(event.key==='ArrowLeft'){
     thanosStand.style.width=thanosWidth
 
-thanosStand.src='/images/thanos-msh-stance.gif';
+thanosStand.src='images/thanos-msh-stance.gif';
 }})
 
 
@@ -47,7 +47,7 @@ thanosStand.src='/images/thanos-msh-stance.gif';
 document.addEventListener('keydown',function(){
 if(event.key==='ArrowUp'){
     thanosStand.style.width='55%'
-    thanosStand.src='/images/than-run.gif';
+    thanosStand.src='images/than-run.gif';
     
 }}
 )
@@ -58,7 +58,7 @@ document.addEventListener('keydown',function(){
 if(event.key==='a'){
     JinStand.style.width=JinWidth
 
-JinStand.src='/images/jin-kisaragi-blazblue-walkforward.gif';
+JinStand.src='images/jin-kisaragi-blazblue-walkforward.gif';
 console.log(JinStand.style.right =(leftjin+=5) +'px' );
 }
 })
@@ -68,7 +68,7 @@ document.addEventListener('keyup',function(){
 if(event.key==='a'){
     JinStand.style.width=JinWidth
 
-JinStand.src='/images/jin-stance.gif';
+JinStand.src='images/jin-stance.gif';
 }
 
 })
@@ -77,7 +77,7 @@ document.addEventListener('keydown',function(){
 if(event.key==='d'){
     JinStand.style.width=JinWidth
 
-JinStand.src='/images/jin-kisaragi-blazblue-walkforward.gif';
+JinStand.src='images/jin-kisaragi-blazblue-walkforward.gif';
 console.log(JinStand.style.right =(leftjin-=5) +'px' );
  if (leftjin<0) {
     console.log(thanosStand.style.left =(leftjin+=5) +'px' );
@@ -89,7 +89,7 @@ console.log(JinStand.style.right =(leftjin-=5) +'px' );
 document.addEventListener('keyup',function(){
 if(event.key==='d'){
     JinStand.style.width=JinWidth
-JinStand.src='/images/jin-stance.gif';
+JinStand.src='images/jin-stance.gif';
 
 }
 
@@ -100,14 +100,14 @@ document.addEventListener('keydown',function(){
 if(event.key==='w'){
     
     JinStand.style.width='42%'
-    JinStand.src='/images/jin-kisaragi-blazblue-loop.gif';
+    JinStand.src='images/jin-kisaragi-blazblue-loop.gif';
 }
 })
 
 document.addEventListener('keydown',function(){
 if(event.key==='s'){
     console.log(JinStand.style.width='14%');
-    JinStand.src='/images/jin-kisaragi-blazblue-colorswap.gif';
+    JinStand.src='images/jin-kisaragi-blazblue-colorswap.gif';
 }
 })
 
